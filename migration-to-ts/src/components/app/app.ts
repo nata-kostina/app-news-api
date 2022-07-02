f61ec962576e3620c2b3d3d175769b0a87f97a6c
Natallia Kostsina <natakostina123@gmail.com>
2022-07-02 16:44:50 +0200

    feat: change callback type

--- migration-to-ts/src/components/app/app.ts
+++ migration-to-ts/src/components/app/app.ts
@@ -1,6 +1,11 @@
 import AppController from '../controller/controller';
-import { AppView } from '../view/appView';
-
+import  AppView from '../view/appView';
+import { ILoadedData } from './../../types/types';
+//const { AppController } = require('../controller/controller');
+//const { AppView } = require('../view/appView');
+interface IData {
+  body: string
+};
 class App {
   private controller: AppController;
   private view: AppView;
@@ -10,9 +15,9 @@ class App {
     }
 
     start(): void {
-      const sources = document.querySelector('.sources') as HTMLElement | null;
-      sources?.addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
-      this.controller.getSources((data) => this.view.drawSources(data));
+      const sources = document.querySelector('.sources') as HTMLElement;
+      sources?.addEventListener('click', (e) => this.controller.getNews(e, (data: ILoadedData) => this.view.drawNews(data)));
+      this.controller.getSources((data: ILoadedData) => this.view.drawSources(data));
     }
 }
 
