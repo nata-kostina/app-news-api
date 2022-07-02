9aab36c33ecd5de6356e289e63860d938856eede
Natallia Kostsina <natakostina123@gmail.com>
2022-07-02 18:00:47 +0200

    feat: migrate appView.js tp appView.ts

---
+++ migration-to-ts/src/components/view/appView.ts
@@ -0,0 +1,24 @@
+import { IGetNewsResponse, IGetSourcesResponse } from '../../types/types';
+import News from './news/news';
+import Sources from './sources/sources';
+
+export class AppView {
+    private readonly news: News;
+    private readonly sources: Sources;
+    constructor() {
+        this.news = new News();
+        this.sources = new Sources();
+    }
+
+    drawNews(data: IGetNewsResponse): void {
+        const values = data?.articles ? data?.articles : [];
+        this.news.draw(values);
+    }
+
+    drawSources(data: IGetSourcesResponse): void {
+        const values = data?.sources ? data?.sources : [];
+        this.sources.draw(values);
+    }
+}
+
+export default AppView;
