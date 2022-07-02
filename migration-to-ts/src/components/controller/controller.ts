d182ffda2a5f20f8aec0f7e15ffb72634f9385be
Natallia Kostsina <natakostina123@gmail.com>
2022-07-02 16:39:57 +0200

    feat: migrate controller.js to controller.ts

---
+++ migration-to-ts/src/components/controller/controller.ts
@@ -0,0 +1,40 @@
+import AppLoader from './appLoader';
+import { ILoadedData } from './../../types/types';
+
+class AppController extends AppLoader {
+    getSources(callback: (data: ILoadedData) => void): void {
+        super.getResp(
+            {
+                endpoint: 'sources',
+            },
+            callback
+        );
+    }
+
+    getNews(e: Event, callback: (data: ILoadedData) => void): void {
+        let target = e.target as HTMLElement;
+        const newsContainer = e.currentTarget as HTMLElement;
+
+        while (target !== newsContainer) {
+            if (target.classList.contains('source__item')) {
+                const sourceId = target.getAttribute('data-source-id') as string;
+                if (newsContainer.getAttribute('data-source') !== sourceId) {
+                    newsContainer.setAttribute('data-source', sourceId);
+                    super.getResp(
+                        {
+                            endpoint: 'everything',
+                            options: {
+                                sources: sourceId,
+                            },
+                        },
+                        callback
+                    );
+                }
+                return;
+            }
+            target = target.parentNode as HTMLElement;
+        }
+    }
+}
+
+export default AppController;
