e0fe71a316ceca5923954fa37aafa2b340ad2097
Natallia Kostsina <natakostina123@gmail.com>
2022-07-02 12:32:06 +0200

    feat: migrate app.js to app.ts

---
+++ migration-to-ts/src/components/app/app.ts
@@ -0,0 +1,19 @@
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+
+class App {
+  private controller: AppController;
+  private view: AppView;
+    constructor() {
+        this.controller = new AppController();
+        this.view = new AppView();
+    }
+
+    start(): void {
+      const sources = document.querySelector('.sources') as HTMLElement | null;
+      sources?.addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data)));
+      this.controller.getSources((data) => this.view.drawSources(data));
+    }
+}
+
+export default App;
