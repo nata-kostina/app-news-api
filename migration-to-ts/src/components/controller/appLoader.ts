3d7998ef8196191f395f3d490ec158463fe93234
Natallia Kostsina <natakostina123@gmail.com>
2022-07-04 23:03:30 +0200

    fix: change URL in appLoader.ts

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: 'bb4e99cf222c467ebdbf93a336c399c0', // получите свой ключ https://newsapi.org/
         });
     }
